6944755779a1d071679fb62d9584bdc2115b0874
mtomison <42246413+mtomison@users.noreply.github.com>
2021-10-14 11:30:17 -0500

    Submission

---
+++ Mission_to_Mars.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Import dependencies (Splinter & BeautifulSoup)
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# Visit the mars nasa news site
+url = 'https://redplanetscience.com'
+browser.visit(url)
+
+# Optional delay for loading the page
+browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('div.list_text')
+
+
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+news_p
+
+
+# ### Featured Images
+
+# In[7]:
+
+
+# Visit url
+url = 'https://spaceimages-mars.com'
+browser.visit(url)
+
+
+# In[8]:
+
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# In[9]:
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+
+# In[10]:
+
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+
+# In[11]:
+
+
+# Use the base URL to create an absolute URL
+img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+img_url
+
+
+# In[12]:
+
+
+df = pd.read_html('https://galaxyfacts-mars.com')[0]
+df.columns=['description','Mars','Earth']
+df.set_index('description', inplace=True)
+df
+
+
+# In[13]:
+
+
+df.to_html()
+
+
+# In[14]:
+
+
+browser.quit()
+
+
+# In[ ]:
+
+
+
+
